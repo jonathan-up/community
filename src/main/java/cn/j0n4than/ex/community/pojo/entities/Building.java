84401c1c0c9cac209080f65588c3fca2359c9398
H0meV <admin@56fkj.cn>
2025-09-16 17:56:45 +0800

    Create Building.java

---
+++ src/main/java/cn/j0n4than/ex/community/pojo/entities/Building.java
@@ -0,0 +1,15 @@
+package cn.j0n4than.ex.community.pojo.entities;
+
+import lombok.Data;
+
+@Data
+public class Building {
+    private Integer id;
+    private String name;
+    private String period;
+    private boolean lift;
+
+    // other table
+    private String villageName;
+    private String property;
+}

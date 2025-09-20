a55e6ffc14e36c48aacc0d374d1268955b5b8cf3
H0meV <admin@56fkj.cn>
2025-09-20 09:30:03 +0800

    登录时返回用户权限

--- src/main/java/cn/j0n4than/ex/community/handlers/AuthHandler.java
+++ src/main/java/cn/j0n4than/ex/community/handlers/AuthHandler.java
@@ -5,17 +5,24 @@ import cn.j0n4than.ex.community.magic.HttpServletResponseEx;
 import cn.j0n4than.ex.community.pojo.ResponseEntity;
 import cn.j0n4than.ex.community.pojo.entities.User;
 import cn.j0n4than.ex.community.pojo.requests.LoginRequest;
+import cn.j0n4than.ex.community.pojo.response.LoginResponse;
+import cn.j0n4than.ex.community.services.MenuService;
 import cn.j0n4than.ex.community.services.UserService;
+import cn.j0n4than.ex.community.services.impl.MenuServiceImpl;
 import cn.j0n4than.ex.community.services.impl.UserServiceImpl;
 import cn.j0n4than.ex.community.utils.JwtUtils;
 import cn.j0n4than.ex.community.utils.TimeUtils;
 
+import java.util.List;
+
 public class AuthHandler {
 
     private final static UserService userService;
+    private final static MenuService menuService;
 
     static {
         userService = new UserServiceImpl();
+        menuService = new MenuServiceImpl();
     }
 
     public static void login(HttpServletRequestEx request, HttpServletResponseEx response) {
@@ -35,6 +42,10 @@ public class AuthHandler {
         user.setSalt(null);
         String token = JwtUtils.crete(user, TimeUtils.HOUR * 10);
 
-        response.json(200, new ResponseEntity<>("登录成功", token));
+        // Find his permissions
+        List<String> permissions = menuService.findPermissions(user.getId());
+        LoginResponse loginResponse = new LoginResponse(token, permissions);
+
+        response.json(200, new ResponseEntity<>("登录成功", loginResponse));
     }
 }
---
+++ src/main/java/cn/j0n4than/ex/community/pojo/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package cn.j0n4than.ex.community.pojo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private List<String> permissions;
+}
